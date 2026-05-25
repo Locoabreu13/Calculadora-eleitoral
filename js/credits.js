a9306cd34a72a9b2fb2e01a32d8aeb30a7748b92
LHPimentel <araujopimentelluiz@gmail.com>
2026-05-25 19:06:39 -0300

    Update credits.js

--- js/credits.js
+++ js/credits.js
@@ -1,13 +1,20 @@
-import { db } from "./firebase-init.js?v=8";
+import { db, auth } from "./firebase-init.js?v=9";
 import { doc, getDocFromServer, runTransaction } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+async function _aguardarToken() {
+  const user = auth.currentUser;
+  if (user) await user.getIdToken();
+}
+
 export async function obterCreditos(uid) {
+  await _aguardarToken();
   const snap = await getDocFromServer(doc(db, "users", uid));
   if (!snap.exists()) return 0;
   return snap.data().credits ?? 0;
 }
 
 export async function debitarCredito(uid) {
+  await _aguardarToken();
   const ref = doc(db, "users", uid);
   try {
     await runTransaction(db, async (t) => {
